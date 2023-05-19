359311d10a35d0f1060f6375934905c2dd20aa47
HoYoung <hoyoung5297@gmail.com>
2023-05-19 16:10:40 +0900

    blackjack_start

--- source/black_jack.c
+++ source/black_jack.c
@@ -1,10 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main()
+int r;
+int end_num;
+
+void start();
+void end();
+int ran();
+
+void main()
 {
-    printf("Hellow, World!");
-    printf("123");
-    printf("q");
+    start();
+    printf("%d", r);
+}
 
-    return 0;
+void start()
+{
+    ran();
+}
+
+int ran()
+{
+    srand(time(NULL));
+    r = rand() % 13; // 1~13까지의 랜덤수 r에 입력
+
+    return r;
+}
+
+void end()
+{
+    switch(end_num)
+    {
+        case 1:
+            printf("승리");
+            break;
+        case 2:
+            printf("버스트!");
+            break;
+        case 3:
+            printf("패배");
+            break;
+    }
 }
